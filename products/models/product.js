4c5fef3bc2077215c2308cb0e7223dc0a523132f
KhadejaZayed <96304678+KhadejaZayed@users.noreply.github.com>
2023-10-18 12:50:54 +0300

    Update product.js

--- products/models/product.js
+++ products/models/product.js
@@ -1,6 +1,5 @@
 const mongoose      = require('mongoose'),
       productSchema = new mongoose.Schema({
-        _id: mongoose.Schema.Types.ObjectId,
         title: { type: String, required: true },
         price: { type: Number, required: true }
       }, {
